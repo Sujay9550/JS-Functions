6a04b14b5bce86a1e0a3928ac0ac9ea4ca3a41b2
Sujay9550 <hardysujay@gmail.com>
2021-12-09 16:53:21 +0530

    Added First Class and Higher Order Functions

--- js/functions.js
+++ js/functions.js
@@ -63,3 +63,38 @@ const newTicketNo = (person) => {
 newTicketNo(mark); // Result - The ticket number is changed to the new number
 console.log(mark);
 busBoard(undefined, mark); // Result - We don't have any bookings with this number (as the ticket no is manipulated by newTicketNo fnctn)
+
+// First Class Function & Higher Order Functions
+
+// First Class Functions
+
+const oneWord = (str) => {
+  return str.replaceAll(" ", "").toLowerCase();
+};
+console.log(oneWord("JavaScript is the best!")); // Result - javascriptisthebest!
+
+const upperFirstWord = (str) => {
+  console.log(str.split(" ")); // Result - ['JavaScript', 'is', 'the', 'best!']
+  const [first, ...others] = str.split(" ");
+  console.log(first, others); // Result - Javascript ['is', 'the', 'best!']
+  return [first.toUpperCase(), ...others].join(" ");
+};
+console.log(upperFirstWord("JavaScript is the best!")); // Result - JAVASCRIPT is the best!
+
+// Higher Order Function - Function accepting Callback Functions
+
+const stringTransformer = (str, fn) => {
+  console.log(`Original String: ${str}`);
+  console.log(`Transformed String: ${fn(str)}`);
+  console.log(`Transformed By Function: ${fn.name}`);
+};
+
+stringTransformer("JavaScript is the best!", oneWord);
+stringTransformer("JavaScript is the best!", upperFirstWord);
+
+// Example
+const high5 = () => {
+  console.log("Hey there!");
+};
+
+document.body.addEventListener("click", high5); // Here high5 is the callback function
